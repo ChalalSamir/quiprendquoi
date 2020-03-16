c765798437cf2dd479466477cd2ff0f6676caec0
ChalalSamir <samir-94@outlook.com>
2020-03-16 16:22:20 +0100

    etape 11

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
+
 const express = require('express');
 const app = express();
 const dotenv = require('dotenv').config();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', function(req, res) {
     res.render('index', { title: 'Qui prend quoi ?' });
@@ -8,9 +12,13 @@ app.get('/', function(req, res) {
 
 app.post('/party', function(req, res) {
     res.send('Post ok !')
+    console.log(req.body);
 });
+
 
 app.listen(process.env.PORT, () => console.log(`Front app listening on port ${process.env.PORT}!`));
 
 app.set('view engine', 'pug');
+
+
 
