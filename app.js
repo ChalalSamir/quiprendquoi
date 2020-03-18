6712554842686f718fb74f424ec7a6e3bfe88ded
ChalalSamir <samir-94@outlook.com>
2020-03-18 12:58:05 +0100

    etape 19 and 20

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const axios = require('axios');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use(express.static('public'));
+
 app.get('/', function(req, res) {
     res.render('index', { title: 'Qui prend quoi ?' });
 });
