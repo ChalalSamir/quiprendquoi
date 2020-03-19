998581d57800a1c441792884b94e96741a1f7ac8
ChalalSamir <samir-94@outlook.com>
2020-03-20 00:12:24 +0100

    etape 28 and 29

--- scripts/clipboard.js
+++ scripts/clipboard.js
@@ -1,25 +1,25 @@
 if (navigator.clipboard) {
-    console.log("Support du presse papier")
-} else {
+    document.querySelectorAll('[data-clipboard]').forEach(($clipboardEl) => {
+        const $button = document.createElement('button');
+        $button.innerHTML = 'Copier';
+        $clipboardEl.parentNode.append($button);
+        $button.addEventListener(
+            'click',
+            copyToClipboard.bind(this, $clipboardEl, $button)
+          );
+      });
+    
+      function copyToClipboard($clipboardEl, $button) {
+        navigator.clipboard
+        .writeText($clipboardEl.getAttribute('data-clipboard'))
+        .then(() => {
+            $button.innerHTML = 'Copié !';
+            setTimeout(() => ($button.innerHTML = 'Copier'), 2000);
+        })
+        .catch((err) => console.warn(err));
+      }
+} 
+else {
     console.warn("Pas de support")
 }
 
-document.querySelectorAll('[data-clipboard]').forEach(($clipboardEl) => {
-    const $button = document.createElement('button');
-    $button.innerHTML = 'Copier';
-    $clipboardEl.parentNode.append($button);
-    $button.addEventListener(
-        'click',
-        copyToClipboard.bind(this, $clipboardEl, $button)
-      );
-  });
-
-  function copyToClipboard($clipboardEl, $button) {
-    navigator.clipboard
-    .writeText($clipboardEl.getAttribute('data-clipboard'))
-    .then(() => {
-        $button.innerHTML = 'Copié !';
-        setTimeout(() => ($button.innerHTML = 'Copier'), 2000);
-    })
-    .catch((err) => console.warn(err));
-  }
